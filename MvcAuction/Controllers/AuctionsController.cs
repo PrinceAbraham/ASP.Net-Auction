f01894943c66fcb99a6d45a5ee8f6c41052b9c33
PrinceAbraham <prince.vabraham@gmail.com>
2016-11-03 15:03:00 -0400

    Index View generated with lists

--- MvcAuction/Controllers/AuctionsController.cs
+++ MvcAuction/Controllers/AuctionsController.cs
@@ -13,7 +13,36 @@ namespace MvcAuction.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var auctions = new[] {
+                new Models.Auction()
+                {
+                Title = "Example Auction #1",
+                Description = "This is 1st Auction",
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddDays(7),
+                StartPrice = 1.00m,
+                CurrentPrice = null
+                },
+                new Models.Auction()
+                {
+                Title = "Example Auction #2",
+                Description = "This is 2nd Auction",
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddDays(7),
+                StartPrice = 1.00m,
+                CurrentPrice = null
+                },
+                new Models.Auction()
+                {
+                Title = "Example Auction #3",
+                Description = "This is 3rd Auction",
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddDays(7),
+                StartPrice = 1.00m,
+                CurrentPrice = 30m
+                }
+            };
+            return View(auctions);
         }
         public ActionResult TempDataDemo()
         {
